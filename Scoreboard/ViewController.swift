4c04dc904883e979dde6d79346180e9b9f3a4a45
vlad <vlad@gmail.com>
2017-02-11 15:01:39 +0000

    Initial Commit

---
+++ Scoreboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scoreboard
+//
+//  Created by Vladimir Koychev on 11/02/2017.
+//  Copyright © 2017 Vladimir Koychev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
